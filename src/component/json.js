35d1b721aa0b7770fcf1d4d1f5a7dab6fea7a5ee
glinet221 <glinet221@gmail.com>
2022-06-23 18:21:42 +0530

    registration form

---
+++ src/component/json.js
@@ -0,0 +1,236 @@
+export const json = {
+    "elements": [
+      {
+        "type": "text",
+        "name": "Date of Survey",
+        "inputType": "date"
+       },
+      {
+        "type": "multipletext",
+        "name": "PM10_AQ_AAQ",
+        "items": [
+         {
+          "name": "Maximum"
+         },
+         {
+          "name": "Minimum"
+         },
+         {
+          "name": "Average"
+         }
+        ]
+        },
+        {
+          "type": "multipletext",
+          "name": "PM2.5_AQ_AAQ",
+          "items": [
+           {
+            "name": "Maximum"
+           },
+           {
+            "name": "Minimum"
+           },
+           {
+            "name": "Average"
+           }
+          ]
+          },
+          {
+            "type": "multipletext",
+            "name": "SO2_AQ_AAQ",
+            "items": [
+             {
+              "name": "Maximum"
+             },
+             {
+              "name": "Minimum"
+             },
+             {
+              "name": "Average"
+             }
+            ]
+            },
+            {
+              "type": "multipletext",
+              "name": "CO_AQ_AAQ",
+              "items": [
+               {
+                "name": "Maximum"
+               },
+               {
+                "name": "Minimum"
+               },
+               {
+                "name": "Average"
+               }
+              ]
+              },
+              {
+                "type": "multipletext",
+                "name": "NO_AQ_AAQ",
+                "items": [
+                 {
+                  "name": "Maximum"
+                 },
+                 {
+                  "name": "Minimum"
+                 },
+                 {
+                  "name": "Average"
+                 }
+                ]
+                },
+                {
+                  "type": "multipletext",
+                  "name": "NO2_AQ_AAQ",
+                  "items": [
+                   {
+                    "name": "Maximum"
+                   },
+                   {
+                    "name": "Minimum"
+                   },
+                   {
+                    "name": "Average"
+                   }
+                  ]
+                  },
+                  {
+                    "type": "multipletext",
+                    "name": "NOX_AQ_AAQ",
+                    "items": [
+                     {
+                      "name": "Maximum"
+                     },
+                     {
+                      "name": "Minimum"
+                     },
+                     {
+                      "name": "Average"
+                     }
+                    ]
+                    },
+                    {
+                      "type": "multipletext",
+                      "name": "NH3_AQ_AAQ",
+                      "items": [
+                       {
+                        "name": "Maximum"
+                       },
+                       {
+                        "name": "Minimum"
+                       },
+                       {
+                        "name": "Average"
+                       }
+                      ]
+                      },
+                      {
+                        "type": "multipletext",
+                        "name": "03_AQ_AAQ",
+                        "items": [
+                         {
+                          "name": "Maximum"
+                         },
+                         {
+                          "name": "Minimum"
+                         },
+                         {
+                          "name": "Average"
+                         }
+                        ]
+                        },
+                        {
+                          "type": "multipletext",
+                          "name": "WS_AQ_KSP",
+                          "items": [
+                           {
+                            "name": "Maximum"
+                           },
+                           {
+                            "name": "Minimum"
+                           },
+                           {
+                            "name": "Average"
+                           }
+                          ]
+                          },
+                          {
+                            "type": "multipletext",
+                            "name": "WD_AQ_KSP",
+                            "items": [
+                             {
+                              "name": "Maximum"
+                             },
+                             {
+                              "name": "Minimum"
+                             },
+                             {
+                              "name": "Average"
+                             }
+                            ]
+                            },
+                            {
+                              "type": "multipletext",
+                              "name": "TC_AQ_KSP",
+                              "items": [
+                               {
+                                "name": "Maximum"
+                               },
+                               {
+                                "name": "Minimum"
+                               },
+                               {
+                                "name": "Average"
+                               }
+                              ]
+                              },
+                              {
+                                "type": "multipletext",
+                                "name": "HR_AQ_KSP",
+                                "items": [
+                                 {
+                                  "name": "Maximum"
+                                 },
+                                 {
+                                  "name": "Minimum"
+                                 },
+                                 {
+                                  "name": "Average"
+                                 }
+                                ]
+                                },
+                                {
+                                  "type": "multipletext",
+                                  "name": "AQ_RG",
+                                  "items": [
+                                   {
+                                    "name": "Maximum"
+                                   },
+                                   {
+                                    "name": "Minimum"
+                                   },
+                                   {
+                                    "name": "Average"
+                                   }
+                                  ]
+                                  },
+          
+                                  {
+                                    "type": "dropdown",
+                                    "name": "Choose a location",
+                                    "choices": [
+                                     "item1",
+                                     "item2",
+                                     "item3"
+                                    ]
+                                   },
+                                   {
+                                    "type": "comment",
+                                    "name": "Write a review"
+                                   }
+                                  ]
+                                  }
+  
+
+   
